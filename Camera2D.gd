b09f5bb11854c2186e9634aaf4af593caae40a07
João Pedro Coelho de Sousa <joaopedro.coelhosousa1@gmail.com>
2024-01-06 15:01:22 -0300

    add archives

---
+++ Camera2D.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")):
+		zoom = Vector2(1,1)
