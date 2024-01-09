9d71ccb7f7a5ae92634fd9fd22debe26d5b33a37
João Pedro Coelho de Sousa <joaopedro.coelhosousa1@gmail.com>
2024-01-09 19:32:20 -0300

    Jump and comments
    Created the jump function and commented the code

--- dino/Player.gd
+++ dino/Player.gd
@@ -12,39 +12,32 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	
-	# Handle Jump.
+	# Handle Jump and Jump Buffer.
 	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")):
 		if is_on_floor():
-			velocity.y = JUMP_VELOCITY
+			pulo()
 		else:
 			$JumpBuffer.start(jump_buffer_time)
-	
+	#Buffered Jump
 	if is_on_floor() && !$JumpBuffer.is_stopped():
 		$JumpBuffer.stop()
-		velocity.y = JUMP_VELOCITY
-		
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-	#	velocity.x = direction * SPEED
-	#else:
-	#	velocity.x = move_toward(velocity.x, 0, SPEED)
-	
+		pulo()
+	#Crouching
 	if Input.is_action_pressed("ui_down"):
 		abaixar()
 	else:
 		levantar()
 	move_and_slide()
 
+func pulo():
+	velocity.y = JUMP_VELOCITY
+	
 func abaixar():
 	get_node("colisao2").set_process(true)
 	get_node("colisao2").visible = true
 	get_node("colisao1").set_process(false)
 	get_node("colisao1").visible = false
-	JUMP_VELOCITY = -350.0
+	JUMP_VELOCITY = -300.0
 
 func levantar():
 	get_node("colisao2").set_process(false)
@@ -52,6 +45,8 @@ func levantar():
 	get_node("colisao1").set_process(true)
 	get_node("colisao1").visible = true
 	JUMP_VELOCITY = -400.0
+
+
 
 
 
