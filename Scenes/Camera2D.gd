4992a4c769232e86b4ec9c118246bfbc3aeef425
João Pedro Coelho de Sousa <joaopedro.coelhosousa1@gmail.com>
2024-01-17 19:54:59 -0300

    Camera Framework
    Ajusted the Camera frame

--- Scenes/Camera2D.gd
+++ Scenes/Camera2D.gd
@@ -10,4 +10,5 @@ func _ready():
 func _process(delta):
 	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")):
 		zoom = Vector2(1,1)
+		position = Vector2(280,-154)
 		Global.start = true
