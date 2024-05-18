03e5e20a64c68acf9d80872b2e6c7b83877dcdf5
João Pedro Coelho de Sousa <joaopedro.coelhosousa1@gmail.com>
2024-05-18 15:12:24 -0300

    New Obstacles

--- Scenes/Spawner.gd
+++ Scenes/Spawner.gd
@@ -5,12 +5,15 @@ var rng = RandomNumberGenerator.new()
 var cactus1 = preload("res://obstacles/cactus1.tscn")
 var cactus2 = preload("res://obstacles/2catus1.tscn")
 var cactus3 = preload("res://obstacles/3cactus1.tscn")
+var cactus4 = preload("res://obstacles/cactus2.tscn")
+var cactus5 = preload("res://obstacles/2cactus2.tscn")
+var cactus6 = preload("res://obstacles/1g2p1g.tscn")
 var aerodactyl = preload("res://obstacles/aerodactyl.tscn")
 var randomObstacle = 0
 var obstaculo
 
 func _on_timeout():
-	randomObstacle = rng.randi_range(1, 5)
+	randomObstacle = rng.randi_range(1, 8)
 	match randomObstacle:
 		1:
 			obstaculo = cactus1.instantiate()
@@ -26,6 +29,15 @@ func _on_timeout():
 			obstaculo.position = Vector2(592,-30)
 		5:
 			obstaculo = cactus3.instantiate()
+			obstaculo.position = Vector2(592,-20)
+		6:
+			obstaculo = cactus4.instantiate()
+			obstaculo.position = Vector2(592,-20)
+		7:
+			obstaculo = cactus5.instantiate()
+			obstaculo.position = Vector2(592,-20)
+		8:
+			obstaculo = cactus6.instantiate()
 			obstaculo.position = Vector2(592,-20)
 	add_child(obstaculo)
 	wait_time = Global.spawnSpeed
