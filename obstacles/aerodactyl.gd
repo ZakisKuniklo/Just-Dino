44396f991a4e5a25d022427ef19029cfcba593ce
João Pedro Coelho de Sousa <joaopedro.coelhosousa1@gmail.com>
2024-05-28 21:11:07 -0300

    Aerodactyl animations

--- obstacles/aerodactyl.gd
+++ obstacles/aerodactyl.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Sprite2D/AnimationPlayer.play("Aerodactyl")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
